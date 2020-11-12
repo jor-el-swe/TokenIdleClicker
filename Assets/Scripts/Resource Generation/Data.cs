3b1e765a023bca3494d77816d404749b30349c8e
Ruben Rådelius <ruben.radelius@student.forsbergsskola.se>
2020-11-12 14:25:49 +0100

    adds Data.cs, adds playerprefs in Generator.cs

---
+++ Assets/Scripts/Resource Generation/Data.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Resource_Generation {
+    [CreateAssetMenu]
+    public class Data : ScriptableObject {
+        [SerializeField] private float productionTime;
+        [SerializeField] private float productionTimeMultiplier;
+        [SerializeField] private float generatedAmount;
+        [SerializeField] private float generatedAmountMultiplier = 3f;
+        [SerializeField] private string generatorName;
+        [SerializeField] private int price;
+        [SerializeField] private float priceMultiplier;
+        [SerializeField] private Resource resource;
+        [SerializeField] private int increaseSpeedThreshold;
+        
+        public float ProductionTime {
+            get => productionTime;
+            set => productionTime = value;
+        }
+    }
+}
+// condition to increase productionTime
+// one click = one revenue
+// generateAmount increased per generator
+// purchasable upgrades that increases generatedAmount
--- Assets/Scripts/Resource Generation/Generator.cs
+++ Assets/Scripts/Resource Generation/Generator.cs
@@ -3,11 +3,22 @@ using UnityEngine.UI;
 
 namespace Resource_Generation
 {
-    public class Generator : MonoBehaviour
-    {
+    public class Generator : MonoBehaviour {
+        
         [SerializeField] private int generatedAmount = 1;
         [SerializeField] private Resource resource;
-
+        private string OwnedKey => $"{this.name}_owned";
+        private string LevelKey => $"{this.name}_level";
+        public int NumberOwned
+        {
+            get => PlayerPrefs.GetInt(OwnedKey, 0);
+            set => PlayerPrefs.SetInt(OwnedKey, value);
+        }
+        public int Level
+        {
+            get => PlayerPrefs.GetInt(LevelKey, 0);
+            set => PlayerPrefs.SetInt(LevelKey, value);
+        }
         public void Generate()
         {
             resource.CurrentAmount += generatedAmount;
