d5d64d786a88f9cff298e9b5c974100e862f8f15
Adam Tapper <adam@tapper.one>
2020-11-12 12:01:10 +0100

    adds generator button

---
+++ Assets/Scripts/Resource Generation/Generator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Resource_Generation
+{
+    public class Generator : MonoBehaviour
+    {
+        [SerializeField] private int generatedAmount = 1;
+        [SerializeField] private Resource resource;
+
+        public void Generate()
+        {
+            resource.CurrentAmount += generatedAmount;
+        }
+    }
+}
