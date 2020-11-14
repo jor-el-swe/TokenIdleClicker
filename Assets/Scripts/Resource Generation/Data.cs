0b84e531fab38a7df63e34f0f645bb7cd5ec0b9a
Ruben Rådelius <ruben.radelius@student.forsbergsskola.se>
2020-11-14 15:27:01 +0100

    adds AutoClicker bool and PlayerPrefs, adds minimumProductionTime to set a limit on how fast production time can be

--- Assets/Scripts/Resource Generation/Data.cs
+++ Assets/Scripts/Resource Generation/Data.cs
@@ -3,41 +3,48 @@
 namespace Resource_Generation {
     [CreateAssetMenu]
     public class Data : ScriptableObject {
-       
+        
+        [SerializeField] private Resource resource;
         [SerializeField] private float productionTime;
         [SerializeField] private float productionTimeMultiplier;
-        [SerializeField] private float generatedAmountMultiplier = 3f;
-        [SerializeField] private string generatorName;
-        [SerializeField] private float priceMultiplier;
+        [SerializeField] private float minimumProductionTime;
         [SerializeField] private int increaseSpeedThreshold;
+        [SerializeField] private int generatedAmount;
+        [SerializeField] private float generatedAmountMultiplier = 3f;
+        [SerializeField] private int price;
+        [SerializeField] private float priceMultiplier;
         [SerializeField] private int levelUpgradePrice;
         [SerializeField] private float levelUpgradeMultiplier;
-        [SerializeField] private int price;
-        [SerializeField] private int generatedAmount;
-        [SerializeField] private Resource resource;
-        
-        public Resource Resource => resource;
-        public float ProductionTime => productionTime;
 
-        public int GetActualPrice(int numberGenerators)
-        {
+        public Resource Resource => resource;
+        private string AutoClickerKey => $"{name}_autoClicker";
+        public bool AutoClickerActive => AutoClicker == 1;
+        public int AutoClicker {
+            get => PlayerPrefs.GetInt(AutoClickerKey, 0);
+            set {
+                //value can only be 0 or 1
+                if (value != 0) value = 1;
+                PlayerPrefs.SetInt(AutoClickerKey, value);
+            }
+        }
+
+        public int GetActualPrice(int numberGenerators) {
             return Mathf.CeilToInt(price * Mathf.Pow(priceMultiplier, numberGenerators-1));
         }
 
-        public int GetActualProductionAmount(int generatorLevel)
-        {
+        public int GetActualProductionAmount(int generatorLevel) {
             return Mathf.CeilToInt(generatedAmount * Mathf.Pow(generatedAmountMultiplier, generatorLevel));
         }
 
-        public int GetActualUpgradePrice(int level)
-        {
+        public int GetActualUpgradePrice(int level) {
             return Mathf.CeilToInt(levelUpgradePrice * Mathf.Pow(levelUpgradeMultiplier, level));
         }
 
-        public float GetActualProductionTime(int numberOwned)
-        {
+        public float GetActualProductionTime(int numberOwned) {
             var increments = numberOwned / increaseSpeedThreshold;
             var actualProductionTime = productionTime * Mathf.Pow(productionTimeMultiplier, increments);
+            if (actualProductionTime < minimumProductionTime)
+                actualProductionTime = minimumProductionTime;
             return actualProductionTime;
         }
     }
