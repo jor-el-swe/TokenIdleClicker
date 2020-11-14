d06733bcbb9bd266e666cf05a40067bc74e4dd31
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-11-14 14:42:42 +0100

    Adds Calculate Time since quit
    Calculate how many seconds since the last time you played the game.

---
+++ Assets/Scripts/CalculateTimeSinceQuit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class CalculateTimeSinceQuit : MonoBehaviour {
+
+    [SerializeField] private TimeSinceQuit timeSinceQuit; //Might change later to public float property and private float
+    void Awake () {
+        var savedDateAndTime = PlayerPrefs.GetString ("OldTimeAndDate");
+        if (savedDateAndTime == "") //Don't use null here!
+            return;
+        var currentDateAndTime = DateTime.Now;
+        var oldDateAndTime = ConvertStringToDateTime (savedDateAndTime);
+        timeSinceQuit.ElapsedTime = Mathf.Max (0f, (float) (currentDateAndTime - oldDateAndTime).TotalSeconds);
+        Debug.Log (timeSinceQuit.ElapsedTime + "s"); //Remove later
+    }
+    void OnApplicationQuit () {
+        PlayerPrefs.SetString ("OldTimeAndDate", DateTime.Now.ToString ());
+    }
+    DateTime ConvertStringToDateTime (string dateTimeString) {
+        var dateTime = DateTime.Parse (dateTimeString);
+        return dateTime;
+    }
+}
