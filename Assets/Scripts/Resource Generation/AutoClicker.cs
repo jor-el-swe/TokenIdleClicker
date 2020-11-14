9e76aaf315eda231353f67fd1d512bf4ecf1a64e
Ruben Rådelius <ruben.radelius@student.forsbergsskola.se>
2020-11-14 15:30:50 +0100

    adds AutoClicker script and Prefab

---
+++ Assets/Scripts/Resource Generation/AutoClicker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Resource_Generation {
+    public class AutoClicker : MonoBehaviour {
+        [SerializeField] private Data data;
+
+        public void Activate() {
+            //TODO add price
+            data.AutoClicker = 1;
+        }
+
+        //temporary method to stop auto clicker for testing purposes
+        public void Stop() {
+            data.AutoClicker = 0;
+        }
+    }
+}
