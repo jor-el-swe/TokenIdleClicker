956b716a719fe0e4b849616c657e96c749a0a228
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-11-16 18:47:08 +0100

    Small performance update for CalculateTimeSinceQuit
    +CalculateTimeSinceQuit:
    Added courotine that updates once every min instead of updating every fixedupdate.
    +Fixed bug in producer where player could gain resources faster than intended when quiting and starting the game.

--- Assets/Scripts/CalculateTimeSinceQuit.cs
+++ Assets/Scripts/CalculateTimeSinceQuit.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class CalculateTimeSinceQuit : MonoBehaviour {
     private void Awake () {
+
         var savedDateAndTime = PlayerPrefs.GetString ("OldTimeAndDate");
+        StartCoroutine (OnUpdateDateTime ());
         if (savedDateAndTime == "") //Don't use null here!
             return;
         var currentDateAndTime = DateTime.Now;
         var oldDateAndTime = ConvertStringToDateTime (savedDateAndTime);
         ProgressSinceQuit.ElapsedTime = Mathf.Max (0f, (float) (currentDateAndTime - oldDateAndTime).TotalSeconds);
     }
-    private void FixedUpdate () {
+    private void OnApplicationQuit () {
         PlayerPrefs.SetString ("OldTimeAndDate", DateTime.Now.ToString ());
+    }
+    private IEnumerator OnUpdateDateTime () {
+        while (true) {
+            PlayerPrefs.SetString ("OldTimeAndDate", DateTime.Now.ToString ());
+            yield return new WaitForSeconds (60);
+        }
     }
     private DateTime ConvertStringToDateTime (string dateTimeString) {
         var dateTime = DateTime.Parse (dateTimeString);
--- Assets/Scripts/ResourceProduction/Producer.cs
+++ Assets/Scripts/ResourceProduction/Producer.cs
@@ -70,11 +70,11 @@ namespace ResourceProduction {
                 Produce ();
         }
         private void ProduceAtStart () {
-            if (!data.AutoClickerActive || ProgressSinceQuit.ElapsedTime < 1)
+            if (!data.AutoClickerActive || ProgressSinceQuit.ElapsedTime < data.GetActualProductionTime (Level))
                 return;
-            var produce = data.GetActualProductionAmount (Level) * (ulong)NumberOwned * 
-                          (ulong)Mathf.RoundToInt (ProgressSinceQuit.ElapsedTime / data.GetActualProductionTime (NumberOwned));
-            
+            var produce = data.GetActualProductionAmount (Level) * (ulong) NumberOwned *
+                (ulong) Mathf.RoundToInt (ProgressSinceQuit.ElapsedTime / data.GetActualProductionTime (NumberOwned));
+
             ProgressSinceQuit.ProducedAmount += produce;
             data.Resource.CurrentAmount += produce;
         }
@@ -83,7 +83,7 @@ namespace ResourceProduction {
             progressBar.FollowProductionTime (data.GetActualProductionTime (NumberOwned), timer);
             if (timer < data.GetActualProductionTime (NumberOwned)) return;
             progressBar.ResetProgressbar ();
-            data.Resource.CurrentAmount += data.GetActualProductionAmount (Level) * (ulong)NumberOwned;
+            data.Resource.CurrentAmount += data.GetActualProductionAmount (Level) * (ulong) NumberOwned;
             timer -= data.GetActualProductionTime (NumberOwned);
             isProducing = false;
         }
