a145ddb015799764da526b741cb45e9170d2353b
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-11-16 17:30:35 +0100

    Changed asset menu names for data and resource

--- Assets/Scripts/Resource/Resource.cs
+++ Assets/Scripts/Resource/Resource.cs
@@ -1,25 +1,21 @@
 ﻿using UnityEngine;
 
 namespace Resource {
-     [CreateAssetMenu]
-     public class Resource : ScriptableObject
-     {
-          public ulong CurrentAmount
-          {
-               get
-               {
-                    var currentValueString = PlayerPrefs.GetString(this.name +"_string", "0");
-                    var currentAmount = System.Convert.ToUInt64(currentValueString);
+     [CreateAssetMenu (fileName = "Resource", menuName = "ScriptableObjects/Resource")]
+     public class Resource : ScriptableObject {
+          public ulong CurrentAmount {
+               get {
+                    var currentValueString = PlayerPrefs.GetString (this.name + "_string", "0");
+                    var currentAmount = System.Convert.ToUInt64 (currentValueString);
                     return currentAmount;
                     //return PlayerPrefs.GetInt(this.name, 0);
                }
-               set
-               {
-                    var currentValueString = value.ToString();
-                    
-                    PlayerPrefs.SetString(this.name +"_string", currentValueString);
-                    
-                   //PlayerPrefs.SetInt(this.name, value);
+               set {
+                    var currentValueString = value.ToString ();
+
+                    PlayerPrefs.SetString (this.name + "_string", currentValueString);
+
+                    //PlayerPrefs.SetInt(this.name, value);
                }
           }
      }
--- Assets/Scripts/ResourceProduction/Data.cs
+++ Assets/Scripts/ResourceProduction/Data.cs
@@ -2,9 +2,9 @@
 using UnityEngine.Serialization;
 
 namespace ResourceProduction {
-    [CreateAssetMenu]
+    [CreateAssetMenu (fileName = "ProductionData", menuName = "ScriptableObjects/ProductionData")]
     public class Data : ScriptableObject {
-        
+
         [SerializeField] private Resource.Resource resource;
         [SerializeField] private float productionTime;
         [SerializeField] private float productionTimeMultiplier;
@@ -23,29 +23,29 @@ namespace ResourceProduction {
         public ulong AutoClickerPrice => autoClickerPrice;
         public bool AutoClickerActive => AutoClicker == 1;
         public int AutoClicker {
-            get => PlayerPrefs.GetInt(AutoClickerKey, 0);
+            get => PlayerPrefs.GetInt (AutoClickerKey, 0);
             set {
                 //value can only be 0 or 1
                 if (value != 0) value = 1;
-                PlayerPrefs.SetInt(AutoClickerKey, value);
+                PlayerPrefs.SetInt (AutoClickerKey, value);
             }
         }
 
-        public ulong GetActualPrice(int numberGenerators) {
-            return (ulong)Mathf.CeilToInt(price * Mathf.Pow(priceMultiplier, numberGenerators));
+        public ulong GetActualPrice (int numberGenerators) {
+            return (ulong) Mathf.CeilToInt (price * Mathf.Pow (priceMultiplier, numberGenerators));
         }
 
-        public ulong GetActualProductionAmount(int generatorLevel) {
-            return (ulong)Mathf.CeilToInt(productionAmount * Mathf.Pow(producedAmountMultiplier, generatorLevel));
+        public ulong GetActualProductionAmount (int generatorLevel) {
+            return (ulong) Mathf.CeilToInt (productionAmount * Mathf.Pow (producedAmountMultiplier, generatorLevel));
         }
 
-        public ulong GetActualUpgradePrice(int level) {
-            return (ulong)Mathf.CeilToInt(levelUpgradePrice * Mathf.Pow(levelUpgradePriceMultiplier, level));
+        public ulong GetActualUpgradePrice (int level) {
+            return (ulong) Mathf.CeilToInt (levelUpgradePrice * Mathf.Pow (levelUpgradePriceMultiplier, level));
         }
 
-        public float GetActualProductionTime(int numberOwned) {
+        public float GetActualProductionTime (int numberOwned) {
             var increments = numberOwned / increaseSpeedThreshold;
-            var actualProductionTime = productionTime * Mathf.Pow(productionTimeMultiplier, increments);
+            var actualProductionTime = productionTime * Mathf.Pow (productionTimeMultiplier, increments);
             if (actualProductionTime < minimumProductionTime)
                 actualProductionTime = minimumProductionTime;
             return actualProductionTime;
