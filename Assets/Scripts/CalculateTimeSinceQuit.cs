07006525136a14a79dffa149c3d34a548bc8d208
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-11-16 19:16:16 +0100

    Code formating change in scripts
    +Removed space after methods.

--- Assets/Scripts/CalculateTimeSinceQuit.cs
+++ Assets/Scripts/CalculateTimeSinceQuit.cs
@@ -3,27 +3,27 @@ using System.Collections;
 using UnityEngine;
 
 public class CalculateTimeSinceQuit : MonoBehaviour {
-    private void Awake () {
+    private void Awake() {
 
-        var savedDateAndTime = PlayerPrefs.GetString ("OldTimeAndDate");
-        StartCoroutine (OnUpdateDateTime ());
+        var savedDateAndTime = PlayerPrefs.GetString("OldTimeAndDate");
+        StartCoroutine(OnUpdateDateTime());
         if (savedDateAndTime == "") //Don't use null here!
             return;
         var currentDateAndTime = DateTime.Now;
-        var oldDateAndTime = ConvertStringToDateTime (savedDateAndTime);
-        ProgressSinceQuit.ElapsedTime = Mathf.Max (0f, (float) (currentDateAndTime - oldDateAndTime).TotalSeconds);
+        var oldDateAndTime = ConvertStringToDateTime(savedDateAndTime);
+        ProgressSinceQuit.ElapsedTime = Mathf.Max(0f, (float) (currentDateAndTime - oldDateAndTime).TotalSeconds);
     }
-    private void OnApplicationQuit () {
-        PlayerPrefs.SetString ("OldTimeAndDate", DateTime.Now.ToString ());
+    private void OnApplicationQuit() {
+        PlayerPrefs.SetString("OldTimeAndDate", DateTime.Now.ToString());
     }
-    private IEnumerator OnUpdateDateTime () {
+    private IEnumerator OnUpdateDateTime() {
         while (true) {
-            PlayerPrefs.SetString ("OldTimeAndDate", DateTime.Now.ToString ());
-            yield return new WaitForSeconds (60);
+            PlayerPrefs.SetString("OldTimeAndDate", DateTime.Now.ToString());
+            yield return new WaitForSeconds(60);
         }
     }
-    private DateTime ConvertStringToDateTime (string dateTimeString) {
-        var dateTime = DateTime.Parse (dateTimeString);
+    private DateTime ConvertStringToDateTime(string dateTimeString) {
+        var dateTime = DateTime.Parse(dateTimeString);
         return dateTime;
     }
 }
