b03798daa568edd94e3a27cb65ea66f053ac3976
Adam Tapper <adam@tapper.one>
2020-11-17 11:34:59 +0100

    adds producerUI

--- Assets/Scripts/ResourceProduction/Producer.cs
+++ Assets/Scripts/ResourceProduction/Producer.cs
@@ -8,40 +8,26 @@ namespace ResourceProduction {
 
         [SerializeField] private Data data;
         [SerializeField] private GameObject produceButton;
-        [SerializeField] private Text buyText;
-        [SerializeField] private Text upgradeText;
-        [SerializeField] private Text numberOwnedText;
-        [SerializeField] private Text levelText;
-        [SerializeField] private Text productionTimeText;
         [SerializeField] private ProgressBar progressBar;
 
+        public Data Data => data;
         private string OwnedKey => $"{data.name}_owned";
-        private string LevelKey => $"{data.name}_level";
-
-        private int NumberOwned {
+        public int NumberOwned {
             get => PlayerPrefs.GetInt(OwnedKey, 0);
-            set => PlayerPrefs.SetInt(OwnedKey, value);
+            private set => PlayerPrefs.SetInt(OwnedKey, value);
         }
-
         public void Buy() {
             if (data.Resource.CurrentAmount < data.GetActualPrice(NumberOwned))
                 return;
             data.Resource.CurrentAmount -= data.GetActualPrice(NumberOwned);
             NumberOwned++;
-            UpdateBuyText();
-            UpdateOwnedText();
         }
-
         public void StartProduction() {
             isProducing = true;
         }
-
         private void Start() {
-            UpdateBuyText();
-            UpdateOwnedText();
             ProduceAtStart();
         }
-
         private void Update() {
             // Disables produceButton when AutoClicker is active and vice versa + checks if you own at least 1 producer
             produceButton.SetActive(!data.AutoClickerActive && NumberOwned > 0);
@@ -67,16 +53,6 @@ namespace ResourceProduction {
             data.Resource.CurrentAmount += data.GetActualProductionAmount(data.Level) * (ulong) NumberOwned;
             timer -= data.GetActualProductionTime(NumberOwned);
             isProducing = false;
-        }
-
-        private void UpdateOwnedText() {
-            numberOwnedText.text = NumberOwned.ToString();
-            productionTimeText.text = data.GetActualProductionTime(NumberOwned).ToString("Production Time: 0.00");
-        }
-
-        private void UpdateBuyText() {
-            buyText.text = $"Buy\n {SuffixHelper.GetString(data.GetActualPrice(NumberOwned))} Tokens ";
-            
         }
     }
 }
---
+++ Assets/Scripts/ResourceProduction/ProducerUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ResourceProduction;
+using UnityEngine.UI;
+
+namespace ResourceProduction
+{
+    public class ProducerUI : MonoBehaviour
+    {
+        
+        [SerializeField] private Text buyText;
+        [SerializeField] private Text productionTimeText;
+        [SerializeField] private Text numberOwnedText;
+        private Data data;
+        private Producer Producer => GetComponent<Producer>();
+
+        void Start() {
+            data = Producer.Data;
+        }
+        void Update() {
+            UpdateOwnedText();
+            UpdateBuyText();
+        }
+        private void UpdateOwnedText() {
+            numberOwnedText.text = Producer.NumberOwned.ToString();
+            productionTimeText.text = data.GetActualProductionTime(Producer.NumberOwned).ToString("Production Time: 0.00");
+        }
+
+        private void UpdateBuyText() {
+            buyText.text = $"Buy\n {SuffixHelper.GetString(data.GetActualPrice(Producer.NumberOwned))} Tokens ";
+        }
+    }
+}
