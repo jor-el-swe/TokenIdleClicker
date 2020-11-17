f5948d4f83ef0a76b7b16f93123537baa05af37a
Ruben Rådelius <ruben.radelius@student.forsbergsskola.se>
2020-11-17 22:03:32 +0100

    adds display Shop Icon logic to ProducerUI.cs and connects placeholder icons to their respective shop in the scene

--- Assets/Scripts/ResourceProduction/ProducerUI.cs
+++ Assets/Scripts/ResourceProduction/ProducerUI.cs
@@ -12,21 +12,25 @@ namespace ResourceProduction
         [SerializeField] private Text buyText;
         [SerializeField] private Text productionTimeText;
         [SerializeField] private Text numberOwnedText;
+        [SerializeField] private Image shopIcon;
         private Data data;
         private Producer Producer => GetComponent<Producer>();
 
-        void Start() {
+        private void UpdateShopIcon() {
+            shopIcon.enabled = Producer.NumberOwned > 0;
+        }
+        private void Start() {
             data = Producer.Data;
         }
-        void Update() {
+        public void FixedUpdate() {
             UpdateOwnedText();
             UpdateBuyText();
+            UpdateShopIcon();
         }
         private void UpdateOwnedText() {
             numberOwnedText.text = Producer.NumberOwned.ToString();
             productionTimeText.text = data.GetActualProductionTime(Producer.NumberOwned).ToString("Production Time: 0.00");
         }
-
         private void UpdateBuyText() {
             buyText.text = $"Buy\n {SuffixHelper.GetString(data.GetActualPrice(Producer.NumberOwned))} Tokens ";
         }
