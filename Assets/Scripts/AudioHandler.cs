dfb607e1c5b4bf04009c77361bba1812207d77f7
Joel Ring <joelwofo@users.noreply.github.com>
2020-11-17 21:41:37 +0100

    adds mute button

---
+++ Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AudioHandler: MonoBehaviour
+{
+    private static Text muteText;
+
+    private void Start()
+    {
+        muteText = GetComponentInChildren<Text>();
+    }
+
+    public static void MuteAllSounds()
+    {
+        if (AudioListener.pause)
+        {
+            AudioListener.pause = false;
+            muteText.text = "Mute All Sounds";
+        }
+        else
+        {
+            AudioListener.pause = true;
+            muteText.text = "Unmute All Sounds";
+        }
+    }
+}
