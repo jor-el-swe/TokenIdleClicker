122bee479a764840bf683411176d5759d5f95a1d
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-11-18 20:52:20 +0100

    Small code refactoring

--- Assets/Scripts/ChangeSinceQuit/ProducedUI.cs
+++ Assets/Scripts/ChangeSinceQuit/ProducedUI.cs
@@ -5,6 +5,10 @@ namespace ChangeSinceQuit {
     public class ProducedUI : MonoBehaviour {
         [SerializeField] private Text textUI;
         [SerializeField] private int destroyTime = 10;
+
+        public void OnClickDestroy() {
+            Destroy(this.gameObject);
+        }
         private void Start() {
             this.Invoke("UpdateText", 0.01f);
         }
@@ -13,15 +17,10 @@ namespace ChangeSinceQuit {
                 Destroy(this.gameObject);
                 return;
             }
-
             textUI.text = $"Tokens produced while gone: \n";
             textUI.text += SuffixHelper.GetString(Data.ProducedAmount);
             textUI.enabled = true;
-
             Destroy(this.gameObject, destroyTime);
-        }
-        public void OnClickDestroy() {
-            Destroy(this.gameObject);
         }
     }
 }
