79544c194ad916f27aa1fa80015df0d39c6832a3
Joel Ring <joelwofo@users.noreply.github.com>
2020-11-18 18:09:46 +0100

    adds cheat button

---
+++ Assets/Scripts/CheatForTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CheatForTest : MonoBehaviour
+{
+    public Resource.Resource resource;
+
+    public void Cheat()
+    {
+        resource.CurrentAmount += 1000000000ul;
+    }
+}
