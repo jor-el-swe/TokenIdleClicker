ea5cbfd68a6c82032b969f20f9b22917833d299e
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-11-19 18:52:09 +0100

    Fixed Exitgame script
    +Developers can now quit in uniity editor.

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 
 public class ExitGame : MonoBehaviour {
-    private void FixedUpdate() {
+    private void Update() {
         if (Input.GetButton("Cancel"))
             Exit();
     }
 
     public void Exit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
