496148b64281db61118fccc7dd4aa54e59218025
Joel Ring <joelwofo@users.noreply.github.com>
2020-11-19 15:15:56 +0100

    starts change on animator

--- Assets/Scripts/ResourceProduction/ProducerUI.cs
+++ Assets/Scripts/ResourceProduction/ProducerUI.cs
@@ -10,9 +10,17 @@ namespace ResourceProduction {
         [SerializeField] private Text numberOwnedText;
         [SerializeField] private Image shopIcon;
         [SerializeField] private Button produceButton;
-        [SerializeField] private Button buyButton;
+        //[SerializeField] private Button buyButton;
+        
+        [SerializeField] private Animator buyButtonAnim;
+
         private Data data;
         private Producer producer => GetComponent<Producer>();
+
+        public void UpdateOnClick()
+        {
+            buyButtonAnim.SetTrigger("Pressed");
+        }
         private void Start() {
             data = producer.Data;
             data = producer.Data;
@@ -45,7 +53,7 @@ namespace ResourceProduction {
         private void UpdateBuyText() {
             (ulong cost, int amount) = data.GetActualBulkPrice(producer.NumberOwned);
             buyText.text = $"Buy {amount}:\n {SuffixHelper.GetString(cost)} Tokens ";
-            if (buyButton != null)
+            if (buyButtonAnim != null)
                 DisableBuyButton(cost);
         }
         private void UpdateShopIcon() {
@@ -64,11 +72,11 @@ namespace ResourceProduction {
         }
         private void DisableBuyButton(ulong cost) {
             if (data.Resource.CurrentAmount >= cost) {
-                buyButton.interactable = true;
-            } else {
-                //buyButton.interactable = false;
-                //TODO check with Henrik how to solve this. I need to push the button to play "nono" sound.
-                buyButton.image.color = Color.red;
+                buyButtonAnim.SetTrigger("Normal");
+            } 
+            else {
+                
+                buyButtonAnim.SetTrigger("Disabled");
             }
         }
     }
