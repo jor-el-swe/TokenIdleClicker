7813546b0a6430fd9afef98889330fe2edd9735f
Joel Ring <joelwofo@users.noreply.github.com>
2020-11-19 12:53:55 +0100

    adds sound to castle buying button

--- Assets/Scripts/Castle/Castle.cs
+++ Assets/Scripts/Castle/Castle.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Castle {
     public class Castle : MonoBehaviour {
         [SerializeField] private Data data;
         public Data Data => data;
         private CastleUI UI => GetComponent<CastleUI>();
+        
+        private AudioHandler audiohandler;
         private string OwnedKey => $"{data.name}_owned";
         public int NumberOwned {
             get => PlayerPrefs.GetInt(OwnedKey, 0);
@@ -14,10 +17,20 @@ namespace Castle {
         public void Buy() {
             var castlePrice = data.GetActualPrice(NumberOwned);
             if (data.Resource.CurrentAmount < castlePrice)
+            {
+                audiohandler.Play("nono");
                 return;
+            }
+            audiohandler.Play("buyStore");
             data.Resource.CurrentAmount -= castlePrice;
             NumberOwned++;
             UI.EnableRandomCastleIcon();
+        }
+
+        private void Start()
+        {
+            //get audioHandler
+            audiohandler = FindObjectOfType<AudioHandler>();
         }
     }
 }
