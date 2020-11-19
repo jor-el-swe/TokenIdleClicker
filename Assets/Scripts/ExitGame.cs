e101368d6b45249374e272d7af652ad8d1887625
Ruben Rådelius <ruben.radelius@student.forsbergsskola.se>
2020-11-19 15:41:13 +0100

    adds ExitGame script

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ExitGame : MonoBehaviour {
+    private void FixedUpdate() {
+        if (Input.GetButton("Cancel"))
+            Exit();
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
