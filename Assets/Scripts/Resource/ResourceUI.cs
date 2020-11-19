91104ad22e0f5e5a9e45d53bd2d56add82584079
Ruben Rådelius <ruben.radelius@student.forsbergsskola.se>
2020-11-19 18:46:08 +0100

    adds user token display text on multiple lines

--- Assets/Scripts/Resource/ResourceUI.cs
+++ Assets/Scripts/Resource/ResourceUI.cs
@@ -1,18 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Resource {
     public class ResourceUI : MonoBehaviour {
         [SerializeField] private Text resourceText;
+        [SerializeField] private Text resourceSuffixText;
         [SerializeField] private Resource resource;
-            // string phrase = "";
-            // string[] words = phrase.Split('\n');
-            // string[0] = 12345;
-            // string[1] = "Billions";
+        
         private void Update()
         {
-            string tokenString = SuffixHelper.GetString(resource.CurrentAmount, true);
-            resourceText.text = $"{tokenString}";
+            var tokenString = SuffixHelper.GetString(resource.CurrentAmount, true);
+            string[] words = tokenString.Split(new string[] { "\n" }, StringSplitOptions.None);
+            resourceText.text = words[0];
+            if (words.Length < 2) {
+                resourceSuffixText.text = "";
+                return;
+            }
+            resourceSuffixText.text = words[1];
         }
     }
 }
