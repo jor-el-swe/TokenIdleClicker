b056a01cb3d0f028c0aed7d8e0548df44519a0b2
Ruben Rådelius <ruben.radelius@student.forsbergsskola.se>
2020-11-19 18:09:12 +0100

    adds new map and new shop icons to main screen

--- Assets/Scripts/Resource/ResourceUI.cs
+++ Assets/Scripts/Resource/ResourceUI.cs
@@ -5,10 +5,10 @@ namespace Resource {
     public class ResourceUI : MonoBehaviour {
         [SerializeField] private Text resourceText;
         [SerializeField] private Resource resource;
-            string phrase = "";
-            string[] words = phrase.Split('\n');
-            string[0] = 12345;
-            string[1] = "Billions";
+            // string phrase = "";
+            // string[] words = phrase.Split('\n');
+            // string[0] = 12345;
+            // string[1] = "Billions";
         private void Update()
         {
             string tokenString = SuffixHelper.GetString(resource.CurrentAmount, true);
